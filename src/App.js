9cccb503fe65dbb7fbf8f2dfa734f5b8a09aeefc
sumit <prajapatisumit009@gmail.com>
2024-07-28 14:41:03 +0530

    Updating Textform.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform';
 function App() {
   return (
     <>
     <Navbar/>
+    <Textform ></Textform>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-export default function Textform() {
+export default function Textform(props) {
   return (
     <div>
-      <form>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Comments</label>
+      <div className="container my-3">
+        <div className="container">
+            <h1> {props.heading} </h1>
+            <p> Enter text to Utilize </p>
+            <textarea className="form-control" placeholder="Enter Text " id="floatingTextarea2" style={{height:'100px'}}></textarea>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
