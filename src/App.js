c539bdaf0432124290e54c7a7275bf5f6e1b0da8
sumit <prajapatisumit009@gmail.com>
2024-07-28 18:20:06 +0530

    Adding mode components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 function App() {
+  const [color,setColor]=useState("light");
+ const tooglemode=()=>{
+  if(color=="light")
+  {
+    setColor("dark");
+    document.body.style.backgroundColor= "#042743";
+  }
+  else
+  {
+    setColor("light");
+    document.body.style.backgroundColor="white";
+  }
+ }
   return (
     <>
-    <Navbar/>
-    <Textform heading="Text Utilizer"></Textform>
+    <Navbar color={color} tooglemode={tooglemode}/>
+    <Textform heading="Text Analyzer" color={color}></Textform>
     </>
   );
 }
