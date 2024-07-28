93c574dd290050ed5877d3705ede7be3142790a0
sumit <prajapatisumit009@gmail.com>
2024-07-28 18:47:24 +0530

    About us content added

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1> About Text Utils </h1>
+      <div className="container">
+        <h2> Introductions </h2>
+        <p> Introduction
+        Text Utils is a versatile and user-friendly web application designed to help you perform a variety of text-related tasks quickly and efficiently. Whether you're a student, a professional, or simply someone who deals with text frequently, Text Utils provides the tools you need to streamline your text processing.</p>
+      </div>
+    </div>
+  )
+}
